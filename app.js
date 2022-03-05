d770999f0266b489ec864ef2bdb06d0e8cac1583
andranikminasyan <100989770+andranikminasyan@users.noreply.github.com>
2022-03-05 10:07:33 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+/*let x=prompt("Hello what is your name?")
+if(x === "Andranik"){
+     alert("Hello Andranik")
+}
+ else if(x !== "Andranik"){
+    alert("ERORR")
+}
+  let y=prompt("How old are you?")
+         
+    if(y >= "18"){
+
+   alert("Lave mech txa es")
+    }if(y < "18"){
+      alert("hl@ poqr es balik jan")
+    }*/
+
+alert("Բարեւ 11-րդ դասարան")
